dae63958a79e5f5c5cf9a7e2e5ec82127337f325
Pauline Rugwevera <prugwevera28@gmail.com>
2023-01-10 21:26:08 +0000

    Customise product and category admin

--- products/models.py
+++ products/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-    # class Meta:
-    #     verbose_name_plural = 'Categories'
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     
-    # return the category name itself
+    
     def __str__(self):
         return self.name
     
