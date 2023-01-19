f664734050613cf3a501f5c91fac68bebdd1f918
Pauline Rugwevera <prugwevera28@gmail.com>
2023-01-19 11:57:38 +0000

    Add success toats notifications

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse,  HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, \
+    get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -9,19 +10,23 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your bag')
     
     else:
         bag[item_id] = quantity
+        messages.success(
+            request, f'{product.name}, added to your shopping bag!')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-    # cart
+
     
 def adjust_bag(request, item_id):
     """
@@ -34,12 +39,14 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(
+            request, f'''Updated {product.name}
+            quantity to {bag[item_id]}!''')
       
     else:
         bag.pop(item_id)
         messages.success(
-            request, f'Removed {product.watch_model} from your bag!')
-
+            request, f'Removed {product.name} from your bag!')
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
@@ -53,7 +60,8 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-      
+        messages.success(
+            request, f'Removed {product.name} from your bag!')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
--- pack_stash/settings.py
+++ pack_stash/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    'bag'
-   ,
+    'bag',
+   
 
 ]
 
@@ -74,11 +74,16 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                 'bag.contexts.bag_contents'
+                'bag.contexts.bag_contents'
             ],
         },
     },
 ]
+
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+
 AUTHENTICATION_BACKENDS = [
     
     # Needed to login by username in Django admin, regardless of `allauth`
@@ -88,6 +93,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
     
 ]
+
 SITE_ID = 1 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
