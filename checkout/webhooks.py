bc7740d371b4c87f448f8414cae4e6fa0abef2ad
Pauline Rugwevera <prugwevera28@gmail.com>
2023-01-27 16:49:28 +0000

    Fix order object in the database to display correct values

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -22,24 +22,31 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
+    
         # Invalid payload
+        print("Payload Error")
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
+       
         # Invalid signature
+        print("Signature Error")
         return HttpResponse(status=400)
     except Exception as e:
+        print("General Error")
         return HttpResponse(content=e, status=400)
 
    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
+    # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': (
+            handler.handle_payment_intent_payment_failed),
     }
 
     # Get the webhook type from Stripe
