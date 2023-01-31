d3debb3ce43dc98fcc4873ab58aa03e3dba8609b
Pauline Rugwevera <prugwevera28@gmail.com>
2023-01-31 17:32:12 +0000

    update order history

--- checkout/views.py
+++ checkout/views.py
@@ -105,8 +105,29 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
+
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address': profile.default_street_address,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
       
-        order_form = OrderForm()
+   
+
+
+
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -128,14 +149,19 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-
+  
 
     profile = UserProfile.objects.get(user=request.user)
     order.user_profile = profile
     order.save()
 
+
+
+  
+
     if save_info:
             profile_data = {
+                'default_full_name': order.full_name,
                 'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
