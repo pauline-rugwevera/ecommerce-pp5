0e9f04341e57981df76640561eb4b81d533b28c0
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-07 13:40:32 +0000

    Deployment test

--- pack_stash/settings.py
+++ pack_stash/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-o8^0siqb$-tr#kqk)__kxc-p%e2#ai_a(b@on^j)opvrr8=8t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['packandstash.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +126,18 @@ WSGI_APPLICATION = 'pack_stash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
