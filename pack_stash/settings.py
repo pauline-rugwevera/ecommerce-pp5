39169a7b660c13be6c93e5735832178274cf9d11
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-07 15:15:39 +0000

    Remove secret key and set debug

--- pack_stash/settings.py
+++ pack_stash/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8^0siqb$-tr#kqk)__kxc-p%e2#ai_a(b@on^j)opvrr8=8t('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['packandstash.herokuapp.com', 'localhost']
 
