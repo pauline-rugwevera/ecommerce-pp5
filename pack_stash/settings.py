c201708e41297e99ee70cb33eae830ba8499ea44
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-10 12:37:04 +0000

    add environment variables for deployment

--- pack_stash/settings.py
+++ pack_stash/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
+
 
 ALLOWED_HOSTS = ['packandstash.herokuapp.com', 'localhost']
 
