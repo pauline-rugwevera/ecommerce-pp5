b5ed2000ebb7179f06e848925fba020116591ff7
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-10 23:56:32 +0000

    update media file

--- pack_stash/settings.py
+++ pack_stash/settings.py
@@ -180,6 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
