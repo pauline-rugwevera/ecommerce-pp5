106bc4efc300f1470371591f5d5934ea78e5f3d6
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-21 13:10:09 +0000

    Add views, urls and render template

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+
+def subscribe(request):
+    """
+    A Viewto handle email when user on submit 
+    form
+    """
+    if request.method == "POST":
+        messages.success(request, "We have received your email.")
+
+    return render(request, "newsletter/newsletter.html")
--- pack_stash/urls.py
+++ pack_stash/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
     path('contactus/', include('contactus.urls')),
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
