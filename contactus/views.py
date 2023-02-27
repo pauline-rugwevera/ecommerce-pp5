bb0d0a50fce5971c1588148d25e2f56fa08690ec
Pauline Rugwevera <prugwevera28@gmail.com>
2023-02-27 12:22:29 +0000

    fix post error handling for contact form

--- contactus/views.py
+++ contactus/views.py
@@ -15,10 +15,11 @@ def contactUs(request):
             form.save()
             messages.success(request, 'Thank you for your message!')
    
-    else:
-        form = ContactForm()
-        messages.warning(request, 'Message not sent. Please try again.')
-
+        else:
+            form = ContactForm()
+            messages.warning(request, 'Message not sent. Please try again.')
+  
+       
     form = ContactForm()
     template = 'contactus/contact_us.html'
     context = {
